42e6f135ce70614e44dbb1ee6a370bb1f695731e
Amos Jun-yeung Ng <me@amos.ng>
2023-03-22 12:17:19 +1100

    Get readthedocs to find langchain_contrib during its build

--- docs/conf.py
+++ docs/conf.py
@@ -1,16 +1,16 @@
 """Configuration file for the Sphinx documentation builder."""
 
+import os
+import sys
+
 import toml
-
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "langchain-contrib"
 copyright = "2023, Amos Ng"
 author = "Amos Ng"
+
+# https://stackoverflow.com/a/60159862
+sys.path.insert(0, os.path.abspath(".."))
 
 with open("../pyproject.toml") as f:
     data = toml.load(f)
